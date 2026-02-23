6b4800a4a4e4ceb86d8709bd05d632e8d5d89915
thhdragon <46762155+thhdragon@users.noreply.github.com>
2026-02-23 02:55:21 -0500

    implement Dns struct with UDP socket initialization in main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+};
+
+use socket2::{Domain, Protocol, Socket, Type};
+
+#[derive(Debug)]
+struct Dns {
+    socket: Socket,
+}
+
+impl Dns {
+    fn new() -> io::Result<Self> {
+        let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+        let address = SocketAddr::from((IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0));
+        let address = address.into();
+        socket.bind(&address)?;
+        println!("{:?}", socket);
+        Ok(Self { socket })
+    }
+}
+
+fn main() -> io::Result<()> {
+    Dns::new()?;
+    Ok(())
 }
